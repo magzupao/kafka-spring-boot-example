55c83ee78ef23715737a6ddafccc2073f4535572
magzupao <mguado@gmail.com>
2023-01-02 15:20:40 +0100

    cambios en application

--- src/main/java/io/tpd/kafkaexample/KafkaExampleApplication.java
+++ src/main/java/io/tpd/kafkaexample/KafkaExampleApplication.java
@@ -1,6 +1,7 @@
 package io.tpd.kafkaexample;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -64,21 +65,23 @@ public class KafkaExampleApplication {
 
     // If you only need one kind of deserialization, you only need to set the
     // Consumer configuration properties. Uncomment this and remove all others below.
-//    @Bean
-//    public Map<String, Object> consumerConfigs() {
-//        Map<String, Object> props = new HashMap<>(
-//                kafkaProperties.buildConsumerProperties()
-//        );
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-//                StringDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-//                JsonDeserializer.class);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG,
-//                "tpd-loggers");
-//
-//        return props;
-//    }
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>(
+                kafkaProperties.buildConsumerProperties()
+        );
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                "tpd-loggers");
 
+        return props;
+    }
+
+// magz
+/*
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         final JsonDeserializer<Object> jsonDeserializer = new JsonDeserializer<>();
@@ -131,4 +134,5 @@ public class KafkaExampleApplication {
         factory.setConsumerFactory(byteArrayConsumerFactory());
         return factory;
     }
+*/
 }
--- src/main/java/io/tpd/kafkaexample/PracticalAdvice.java
+++ src/main/java/io/tpd/kafkaexample/PracticalAdvice.java
@@ -2,6 +2,32 @@ package io.tpd.kafkaexample;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-record PracticalAdvice(@JsonProperty("message") String message,
-                       @JsonProperty("identifier") int identifier) {
+//record PracticalAdvice(@JsonProperty("message") String message,
+//                       @JsonProperty("identifier") int identifier) {
+//}
+public class PracticalAdvice {
+    private final String message;
+    private final int identifier;
+
+    public PracticalAdvice(@JsonProperty("message") final String message,
+                           @JsonProperty("identifier") final int identifier) {
+        this.message = message;
+        this.identifier = identifier;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public String toString() {
+        return "PracticalAdvice::toString() {" +
+                "message='" + message + '\'' +
+                ", identifier=" + identifier +
+                '}';
+    }
 }
